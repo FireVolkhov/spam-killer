ce8cbb2b9f7363177a556bf139c43d5032e912fc
FireVolkhov <firevolkhov@gmail.com>
2016-07-12 17:20:54 +0500

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+setInterval(function(){
+    var spam = document.querySelector('.slider__item.slider__item_advert.slider__item_shown');
+
+    if (spam) {
+        window.location = '';
+    }
+}, 500);
